b1e6e8caca0215c0cb0ac5ae096cbd28d61bf4e3
Nicolò Ginatta <nicolo@proximarobotics.com>
2021-09-28 18:05:06 +0200

    First vec3 trait sketch with tests

---
+++ include/nkgt/traits/vectors.hpp
@@ -0,0 +1,54 @@
+#ifndef _NKGT_TRAITS_VECTORS_
+#define _NKGT_TRAITS_VECTORS_
+
+#include <type_traits>
+
+namespace nkgt {
+
+template<typename, typename = void>
+struct is_vec3_like : std::false_type {};
+
+/**
+ * @brief A type trait to check if a given type T is vector3-like by exposing
+ * public data members named x, y and z. Note that static and/or const data
+ * members are acceptable to be "vector3-like".
+ * 
+ * @tparam T The type you wish to know if is vector3-like.
+ */
+template<typename T>
+struct is_vec3_like<
+    T,
+    std::void_t<
+        // Using decltype(std::declval<T>().x) instead of decltype(&T::x)
+        // allows to detect also static data members.
+        decltype(std::declval<T>().x),
+        decltype(std::declval<T>().y),
+        decltype(std::declval<T>().z)
+    >
+> : std::bool_constant<
+        // Check if they are data member and not methods
+        std::is_member_object_pointer_v<decltype(std::declval<T>().x)> &&
+        std::is_member_object_pointer_v<decltype(std::declval<T>().y)> &&
+        std::is_member_object_pointer_v<decltype(std::declval<T>().z)> &&
+        // Check that the types are uniform across members
+        std::is_same_v<decltype(std::declval<T>().x), decltype(std::declval<T>().y)> &&
+        std::is_same_v<decltype(std::declval<T>().y), decltype(std::declval<T>().z)> &&
+        // Check that the type are numerical. This still accepts char and the
+        // like which I think is a mistake!
+        std::is_arithmetic_v<decltype(std::declval<T>().x)> &&
+        std::is_arithmetic_v<decltype(std::declval<T>().y)> &&
+        std::is_arithmetic_v<decltype(std::declval<T>().z)>
+    >
+{};
+
+/**
+ * @brief A convenience definition to access the [[is_vec3_like]] boolean value
+ * 
+ * @tparam T The type you wish to know if is vector3-like
+ */
+template<typename T>
+inline constexpr bool is_vec3_like_v = is_vec3_like<T>::value;
+
+} // namespace nkgt
+
+#endif // _NKGT_TRAITS_VECTORS_
---
+++ test/traits/vectors.test.hpp
@@ -0,0 +1,46 @@
+#include <catch2/catch.hpp>
+#include <nkgt/traits/vectors.hpp>
+
+#include <string>
+
+struct vec3_int { int x, y, z; };
+struct vec3_int_wrong_name { int x, y, w; };
+struct vec3_double { double x, y, z; };
+struct vec3_mixed_type { int x, y; double z; };
+
+struct vec3_static_z { int x, y; static int z; };
+int vec3_static_z::z = 3;
+
+struct vec3_const_z { int x, y; const int z; };
+struct vec3_non_numeric { std::string x, y, z; };
+struct vec3_more_members { int x, y, z, a; };
+struct vec3_xyz_methods { int x(); int y(); int z(); };
+
+TEST_CASE("is_vec3_like traits recognizes the correct types", "[traits-vector]") {
+    SECTION("Numerical data members are acceptable") {
+        REQUIRE(nkgt::is_vec3_like_v<vec3_int>);
+        REQUIRE(nkgt::is_vec3_like_v<vec3_double>);
+        REQUIRE(nkgt::is_vec3_like_v<vec3_static_z>);
+        REQUIRE(nkgt::is_vec3_like_v<vec3_const_z>);
+    }
+
+    SECTION("Non-numerical data members are rejected") {
+        REQUIRE(!nkgt::is_vec3_like_v<vec3_non_numeric>);
+    }
+
+    SECTION("Wrong naming is rejected") {
+        REQUIRE(!nkgt::is_vec3_like_v<vec3_int_wrong_name>);
+    }
+
+    SECTION("Non-static data members must have uniform numerical type.") {
+        REQUIRE(!nkgt::is_vec3_like_v<vec3_mixed_type>);
+    }
+
+    SECTION("Member methods with the same name are rejected") {
+        REQUIRE(!nkgt::is_vec3_like_v<vec3_xyz_methods>);
+    }
+
+    SECTION("Additional data members are ignored") {
+        REQUIRE(nkgt::is_vec3_like_v<vec3_more_members>);
+    }
+}
