e23354f1985a84430701c2fbb197751667e2ffea
Nicolò Ginatta <nicolo.ginatta@gmail.com>
2021-09-30 10:43:29 +0200

    Noisy class implementation

--- include/nkgt/noisy.hpp
+++ include/nkgt/noisy.hpp
@@ -1,14 +1,29 @@
 #ifndef _NKGT_NOISY_
 #define _NKGT_NOISY_
 
+#include <nkgt/type_support.hpp>
+#include <iostream>
+
 namespace nkgt {
 
+template<typename T>
 class Noisy {
 public:
-    Noisy();
-    Noisy(const Noisy&);
-    Noisy(Noisy&&);
-    ~Noisy();
+    Noisy() {
+        std::cout << type_name_v<T> << " constructor invoked.\n";
+    };
+
+    Noisy(const Noisy&) {
+        std::cout << type_name_v<T> << " copy-constructor invoked.\n";
+    };
+
+    Noisy(Noisy&&) {
+        std::cout << type_name_v<T> << " move-constructor invoked.\n";
+    };
+
+    ~Noisy() {
+        std::cout << type_name_v<T> << " destructor invoked.\n";
+    };
 };
 
 }
